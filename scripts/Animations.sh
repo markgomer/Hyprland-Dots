4af669572d127bdf5578355a4b457f00ca6fd908
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-08-15 13:49:34 -0300

    [chore] move some comments, adjust identation, reword variables

--- scripts/Animations.sh
+++ scripts/Animations.sh
@@ -3,7 +3,7 @@
 # For applying Animations from different users
 
 # Check if rofi is already running
-if pidof rofi > /dev/null; then
+if pidof rofi >/dev/null; then
   pkill rofi
 fi
 
@@ -22,10 +22,16 @@ chosen_file=$(echo "$animations_list" | rofi -i -dmenu -config $rofi_theme -mesg
 
 # Check if a file was selected
 if [[ -n "$chosen_file" ]]; then
-    full_path="$animations_dir/$chosen_file.conf"    
-    cp "$full_path" "$UserConfigs/UserAnimations.conf"    
-    notify-send -u low -i "$iDIR/ja.png" "$chosen_file" "Hyprland Animation Loaded"
+  full_path="$animations_dir/$chosen_file.conf"
+  cp "$full_path" "$UserConfigs/UserAnimations.conf"
+  notify-send -u low -i "$iDIR/ja.png" "$chosen_file" "Hyprland Animation Loaded"
 fi
+
+# if [[ -n "$chosen_file" ]]; then
+#     full_path="$HOME/.config/hypr/animations/$chosen_file.conf"
+#     cp "$full_path" "$UserConfigs/UserAnimations.conf"
+#     notify-send -u low -i "$iDIR/ja.png" "$chosen_file" "Hyprland Animation Loaded"
+# fi
 
 sleep 1
 "$SCRIPTSDIR/RefreshNoWaybar.sh"
